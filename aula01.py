64d237ba7742237f6ae39dea0ff2eeb6813f6f71
YanZN <yanrobim@gmail.com>
2024-12-14 11:27:50 -0300

    sabendo mais sobre variaveis

---
+++ aula01.py
@@ -0,0 +1,9 @@
+# Aprendendo criando programas, somas etc
+
+#print(input("digite um primeiro número^:")) + int(input("digite um segundo numero:")))
+
+numero = 3
+numero_decimal = 5.90
+nome_de_usuario = "Zézin"
+print(type(nome_de_usuario))
+verdadeiro = True
