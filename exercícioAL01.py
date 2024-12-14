56b91b82c3b8eb344e08cb2829bec5bfe56dacff
YanZN <yanrobim@gmail.com>
2024-12-14 11:30:56 -0300

    meu primeiro exercicio

---
+++ exercícioAL01.py
@@ -0,0 +1,16 @@
+# Aprendendo criando programas, somas etc
+
+#print(int(input("digite um primeiro número^:")) + int(input("digite um segundo numero:")))
+
+#numero = 3
+#numero_decimal = 5.90
+#nome_de_usuario = "Zézin"
+#print(type(nome_de_usuario))
+#verdadeiro = True 
+
+nome_de_usúario = input("Digite seu no0me:")
+salario_do_usuario = float(input("Digite seu salario:"))
+bonus_usúario = (float(input("Digite o bonús recebido:")))
+CONSTANTE_BONUS = 1000
+valor_do_bonus = CONSTANTE_BONUS + salario_do_usuario * bonus_usúario
+print(f"o usúario {nome_de_usúario} possui um valor total de  {valor_do_bonus}")
